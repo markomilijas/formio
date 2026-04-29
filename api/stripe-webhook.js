edf0e209d4bd0af5e5537a9c96b5321547fd97f5
markomilijas <marko.milijas@otvorillc.com>
2026-04-29 15:39:42 +0200

    Create stripe-webhook.js

---
+++ api/stripe-webhook.js
@@ -0,0 +1,6 @@
+// /api/stripe-webhook.js
+// Placeholder - implementiraćemo u Koraku 4 (posle frontend integracije)
+
+export default async function handler(req, res) {
+  return res.status(200).json({ received: true });
+}
